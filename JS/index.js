cd0057ba4bb46af61cfd705f22556670dba4926a
aimansae <saeedaimann@gmail.com>
2023-08-01 22:22:36 +0200

    Fix toggle menu styling

--- JS/index.js
+++ JS/index.js
@@ -1,11 +1,35 @@
 const togglemenu = document.getElementsByClassName("toggle-menu")[0]
 const navbarLinks = document.getElementsByClassName('nav-links')[0]
+const signupStyle = document.getElementsByClassName('signup-style')[0]
+const svgContainer = document.getElementsByClassName('instapro-logo')[0];
+const header = document.getElementsByClassName('header')[0]
 
 
 togglemenu.addEventListener('click', () =>{
     navbarLinks.classList.toggle('active');
-    navbarLinks.classList.add('togglestyle')
+    navbarLinks.classList.add('togglestyle');    
+    togglemenu.style.color = 'black';
+    togglemenu.style.border = '1px solid #3F27BA';
+    header.style.backgroundColor ='#fff';
+
+    signupStyle.style.border = 'none';
+
+    
 })
+
+
+// for logo image
+togglemenu.addEventListener('click', () =>{
+
+  const logoImg = document.createElement('img');
+  logoImg.classList.add('newLogo')
+  logoImg.src = './images/logo-blue.png';
+  logoImg.alt = 'Logo Image';
+
+  svgContainer.innerHTML = '';
+  svgContainer.appendChild(logoImg);
+})
+
 
 // expand more jobs button
 var expandButton = document.getElementById("expandButton");
