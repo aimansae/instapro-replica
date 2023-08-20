b84db686f9c1548229c4de094e531f9396de388b
aimansae <saeedaimann@gmail.com>
2023-08-20 14:20:54 +0200

    add style to logo and nav if active

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,8 @@
+const logo = document.getElementsByClassName('logoblue')[0]
 const togglemenu = document.getElementsByClassName("toggle-menu")[0]
+const toggleMenuX = document.getElementsByClassName('iconx')[0]
+const toggleMenuBars = document.getElementsByClassName('icon-bars')[0]
+
 const navbarLinks = document.getElementsByClassName('nav-links')[0]
 const signupStyle = document.getElementsByClassName('signup-style')[0]
 const svgContainer = document.getElementsByClassName('instapro-logo')[0];
@@ -7,28 +11,30 @@ const header = document.getElementsByClassName('header')[0]
 
 togglemenu.addEventListener('click', () =>{
     navbarLinks.classList.toggle('active');
-    navbarLinks.classList.add('togglestyle');    
-    togglemenu.style.color = 'black';
-    togglemenu.style.border = '1px solid #3F27BA';
-    header.style.backgroundColor ='#fff';
-
+    togglemenu.classList.toggle("navbarActive")
+    header.classList.toggle("headerActiveBackground")
+    toggleMenuBars.classList.toggle('activeBars')
+    toggleMenuX.classList.toggle('activeX')
     signupStyle.style.border = 'none';
+    logo.classList.toggle('activeLogo')
+
+    
 
     
 })
 
 
 // for logo image
-togglemenu.addEventListener('click', () =>{
+// togglemenu.addEventListener('click', () =>{
 
-  const logoImg = document.createElement('img');
-  logoImg.classList.add('newLogo')
-  logoImg.src = './images/logo-blue.png';
-  logoImg.alt = 'Logo Image';
+//   const logoImg = document.createElement('img');
+//   logoImg.classList.add('newLogo')
+//   logoImg.src = './images/logo-blue.png';
+//   logoImg.alt = 'Logo Image';
 
-  svgContainer.innerHTML = '';
-  svgContainer.appendChild(logoImg);
-})
+//   svgContainer.innerHTML = '';
+//   svgContainer.appendChild(logoImg);
+// })
 
 
 // expand more jobs button
