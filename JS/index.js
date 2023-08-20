2f1a124aa3029a8e57e062cd42d00ef59f8e5a78
aimansae <saeedaimann@gmail.com>
2023-08-20 18:58:06 +0200

    Format and remove comments

--- JS/index.js
+++ JS/index.js
@@ -1,27 +1,49 @@
-const logo = document.getElementsByClassName('logoblue')[0]
-const togglemenu = document.getElementsByClassName("toggle-menu")[0]
-const toggleMenuX = document.getElementsByClassName('iconx')[0]
-const toggleMenuBars = document.getElementsByClassName('icon-bars')[0]
+const logo = document.getElementsByClassName("logoblue")[0];
+const menuButton = document.getElementsByClassName("toggle-menu")[0];
+const toggleMenuX = document.getElementsByClassName("iconx")[0];
+const toggleMenuBars = document.getElementsByClassName("icon-bars")[0];
 
-const navbarLinks = document.getElementsByClassName('nav-links')[0]
-const signupStyle = document.getElementsByClassName('signup-style')[0]
-const svgContainer = document.getElementsByClassName('instapro-logo')[0];
-const header = document.getElementsByClassName('header')[0]
+const navbarLinks = document.getElementsByClassName("nav-links")[0];
+const signupStyle = document.getElementsByClassName("signup-style")[0];
+const svgContainer = document.getElementsByClassName("instapro-logo")[0];
+const header = document.getElementsByClassName("header")[0];
 
+menuButton.addEventListener("click", (e) => {
+  navbarLinks.classList.toggle("active");
+  menuButton.classList.toggle("navbarActive");
+  header.classList.toggle("headerActiveBackground");
+  toggleMenuBars.classList.toggle("activeBars");
+  toggleMenuX.classList.toggle("activeX");
+  signupStyle.style.border = "none";
+  logo.classList.toggle("activeLogo");
+});
 
-togglemenu.addEventListener('click', () =>{
-    navbarLinks.classList.toggle('active');
-    togglemenu.classList.toggle("navbarActive")
-    header.classList.toggle("headerActiveBackground")
-    toggleMenuBars.classList.toggle('activeBars')
-    toggleMenuX.classList.toggle('activeX')
-    signupStyle.style.border = 'none';
-    logo.classList.toggle('activeLogo')
+// to close togglemenu on click
+document.addEventListener("click", (e) => {
+  const target = e.target;
 
-    
+  if (!menuButton.contains(target) && !navbarLinks.contains(target)) {
+    navbarLinks.classList.remove("active");
+    menuButton.classList.remove("navbarActive");
+    toggleMenuBars.classList.remove("activeBars");
+    header.classList.remove("headerActiveBackground");
 
-    
-})
+    toggleMenuX.classList.remove("activeX");
+    signupStyle.style.border = "";
+    logo.classList.remove("activeLogo");
+  }
+});
+
+// expand more jobs button
+var expandButton = document.getElementById("expandButton");
+var container = document.querySelector(".expand-hide");
+
+expandButton.addEventListener("click", function () {
+  container.classList.toggle("show");
+
+  expandButton.remove();
+  console.log("clicked");
+});
 
 
 // for logo image
@@ -35,15 +57,3 @@ togglemenu.addEventListener('click', () =>{
 //   svgContainer.innerHTML = '';
 //   svgContainer.appendChild(logoImg);
 // })
-
-
-// expand more jobs button
-var expandButton = document.getElementById("expandButton");
-var container = document.querySelector(".expand-hide");
-
-expandButton.addEventListener("click", function() {
-  container.classList.toggle("show");
-
-  expandButton.remove();
-  console.log('clicked')
-});
